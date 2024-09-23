54a2d1a9c2933194482ed3774e1547ce45f6fe8e
RandallMan07 <adriamartinmartin@gmail.com>
2024-09-23 09:53:22 +0200

    Paradigm changed -> service-worker added

--- sumPdf/popup.js
+++ sumPdf/popup.js
@@ -1,3 +1,16 @@
-function onClick() {
-    
+document.getElementById("summarizer_btn").addEventListener("click", summarize)
+
+async function summarize() {
+    chrome.tab.query({ active: true, currentWindow: true}, (tabs) => {
+        const activeTab = tabs[0];
+        const tabURL = activeTab.url;
+
+        if (url && url.endsWith('.pdf'))
+        {
+            alert("This is a pdf file!")
+        }
+        else {
+            alert("This isnt a pdf!!! :(")
+        }
+    })
 }
